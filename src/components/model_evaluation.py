0a07f66a604655e05b24ea42d3f6652d34ed5b40
Shashwatnig001 <shashwatnigam25@gmail.com>
2026-05-05 16:04:31 +0530

    aws connection added

--- src/components/model_evaluation.py
+++ src/components/model_evaluation.py
@@ -1,76 +1,55 @@
-import sys
-import numpy as np
-import pandas as pd
-from imblearn.combine import SMOTEENN
-from sklearn.pipeline import Pipeline
-from sklearn.preprocessing import StandardScaler, MinMaxScaler
-from sklearn.compose import ColumnTransformer
-
-from src.constants import TARGET_COLUMN, SCHEMA_FILE_PATH, CURRENT_YEAR
-from src.entity.config_entity import DataTransformationConfig
-from src.entity.artifact_entity import DataTransformationArtifact, DataIngestionArtifact, DataValidationArtifact
+from src.entity.config_entity import ModelEvaluationConfig
+from src.entity.artifact_entity import ModelTrainerArtifact, DataIngestionArtifact, ModelEvaluationArtifact
+from sklearn.metrics import f1_score
 from src.exception import MyException
+from src.constants import TARGET_COLUMN
 from src.logger import logging
-from src.utils.main_utils import save_object, save_numpy_array_data, read_yaml_file
+from src.utils.main_utils import load_object
+import sys
+import pandas as pd
+from typing import Optional
+from src.entity.s3_estimator import Proj1Estimator
+from dataclasses import dataclass
+
+@dataclass
+class EvaluateModelResponse:
+    trained_model_f1_score: float
+    best_model_f1_score: float
+    is_model_accepted: bool
+    difference: float
 
 
-class DataTransformation:
-    def __init__(self, data_ingestion_artifact: DataIngestionArtifact,
-                 data_transformation_config: DataTransformationConfig,
-                 data_validation_artifact: DataValidationArtifact):
+class ModelEvaluation:
+
+    def __init__(self, model_eval_config: ModelEvaluationConfig, data_ingestion_artifact: DataIngestionArtifact,
+                 model_trainer_artifact: ModelTrainerArtifact):
         try:
+            self.model_eval_config = model_eval_config
             self.data_ingestion_artifact = data_ingestion_artifact
-            self.data_transformation_config = data_transformation_config
-            self.data_validation_artifact = data_validation_artifact
-            self._schema_config = read_yaml_file(file_path=SCHEMA_FILE_PATH)
+            self.model_trainer_artifact = model_trainer_artifact
         except Exception as e:
-            raise MyException(e, sys)
-
-    @staticmethod
-    def read_data(file_path) -> pd.DataFrame:
-        try:
-            return pd.read_csv(file_path)
-        except Exception as e:
-            raise MyException(e, sys)
-
-    def get_data_transformer_object(self) -> Pipeline:
-        """
-        Creates and returns a data transformer object for the data, 
-        including gender mapping, dummy variable creation, column renaming,
-        feature scaling, and type adjustments.
-        """
-        logging.info("Entered get_data_transformer_object method of DataTransformation class")
-
-        try:
-            # Initialize transformers
-            numeric_transformer = StandardScaler()
-            min_max_scaler = MinMaxScaler()
-            logging.info("Transformers Initialized: StandardScaler-MinMaxScaler")
-
-            # Load schema configurations
-            num_features = self._schema_config['num_features']
-            mm_columns = self._schema_config['mm_columns']
-            logging.info("Cols loaded from schema.")
-
-            # Creating preprocessor pipeline
-            preprocessor = ColumnTransformer(
-                transformers=[
-                    ("StandardScaler", numeric_transformer, num_features),
-                    ("MinMaxScaler", min_max_scaler, mm_columns)
-                ],
-                remainder='passthrough'  # Leaves other columns as they are
-            )
-
-            # Wrapping everything in a single pipeline
-            final_pipeline = Pipeline(steps=[("Preprocessor", preprocessor)])
-            logging.info("Final Pipeline Ready!!")
-            logging.info("Exited get_data_transformer_object method of DataTransformation class")
-            return final_pipeline
-
-        except Exception as e:
-            logging.exception("Exception occurred in get_data_transformer_object method of DataTransformation class")
             raise MyException(e, sys) from e
 
+    def get_best_model(self) -> Optional[Proj1Estimator]:
+        """
+        Method Name :   get_best_model
+        Description :   This function is used to get model from production stage.
+        
+        Output      :   Returns model object if available in s3 storage
+        On Failure  :   Write an exception log and then raise an exception
+        """
+        try:
+            bucket_name = self.model_eval_config.bucket_name
+            model_path=self.model_eval_config.s3_model_key_path
+            proj1_estimator = Proj1Estimator(bucket_name=bucket_name,
+                                               model_path=model_path)
+
+            if proj1_estimator.is_model_present(model_path=model_path):
+                return proj1_estimator
+            return None
+        except Exception as e:
+            raise  MyException(e,sys)
+        
     def _map_gender_column(self, df):
         """Map Gender column to 0 for Female and 1 for Male."""
         logging.info("Mapping 'Gender' column to binary values")
@@ -94,83 +73,80 @@ class DataTransformation:
             if col in df.columns:
                 df[col] = df[col].astype('int')
         return df
-
+    
     def _drop_id_column(self, df):
         """Drop the 'id' column if it exists."""
         logging.info("Dropping 'id' column")
-        drop_col = self._schema_config['drop_columns']
-        if drop_col in df.columns:
-            df = df.drop(drop_col, axis=1)
+        if "_id" in df.columns:
+            df = df.drop("_id", axis=1)
         return df
 
-    def initiate_data_transformation(self) -> DataTransformationArtifact:
+    def evaluate_model(self) -> EvaluateModelResponse:
         """
-        Initiates the data transformation component for the pipeline.
+        Method Name :   evaluate_model
+        Description :   This function is used to evaluate trained model 
+                        with production model and choose best model 
+        
+        Output      :   Returns bool value based on validation results
+        On Failure  :   Write an exception log and then raise an exception
         """
         try:
-            logging.info("Data Transformation Started !!!")
-            if not self.data_validation_artifact.validation_status:
-                raise Exception(self.data_validation_artifact.message)
+            test_df = pd.read_csv(self.data_ingestion_artifact.test_file_path)
+            x, y = test_df.drop(TARGET_COLUMN, axis=1), test_df[TARGET_COLUMN]
 
-            # Load train and test data
-            train_df = self.read_data(file_path=self.data_ingestion_artifact.trained_file_path)
-            test_df = self.read_data(file_path=self.data_ingestion_artifact.test_file_path)
-            logging.info("Train-Test data loaded")
+            logging.info("Test data loaded and now transforming it for prediction...")
 
-            input_feature_train_df = train_df.drop(columns=[TARGET_COLUMN], axis=1)
-            target_feature_train_df = train_df[TARGET_COLUMN]
+            x = self._map_gender_column(x)
+            x = self._drop_id_column(x)
+            x = self._create_dummy_columns(x)
+            x = self._rename_columns(x)
 
-            input_feature_test_df = test_df.drop(columns=[TARGET_COLUMN], axis=1)
-            target_feature_test_df = test_df[TARGET_COLUMN]
-            logging.info("Input and Target cols defined for both train and test df.")
+            trained_model = load_object(file_path=self.model_trainer_artifact.trained_model_file_path)
+            logging.info("Trained model loaded/exists.")
+            trained_model_f1_score = self.model_trainer_artifact.metric_artifact.f1_score
+            logging.info(f"F1_Score for this model: {trained_model_f1_score}")
 
-            # Apply custom transformations in specified sequence
-            input_feature_train_df = self._map_gender_column(input_feature_train_df)
-            input_feature_train_df = self._drop_id_column(input_feature_train_df)
-            input_feature_train_df = self._create_dummy_columns(input_feature_train_df)
-            input_feature_train_df = self._rename_columns(input_feature_train_df)
+            best_model_f1_score=None
+            best_model = self.get_best_model()
+            if best_model is not None:
+                logging.info(f"Computing F1_Score for production model..")
+                y_hat_best_model = best_model.predict(x)
+                best_model_f1_score = f1_score(y, y_hat_best_model)
+                logging.info(f"F1_Score-Production Model: {best_model_f1_score}, F1_Score-New Trained Model: {trained_model_f1_score}")
+            
+            tmp_best_model_score = 0 if best_model_f1_score is None else best_model_f1_score
+            result = EvaluateModelResponse(trained_model_f1_score=trained_model_f1_score,
+                                           best_model_f1_score=best_model_f1_score,
+                                           is_model_accepted=trained_model_f1_score > tmp_best_model_score,
+                                           difference=trained_model_f1_score - tmp_best_model_score
+                                           )
+            logging.info(f"Result: {result}")
+            return result
 
-            input_feature_test_df = self._map_gender_column(input_feature_test_df)
-            input_feature_test_df = self._drop_id_column(input_feature_test_df)
-            input_feature_test_df = self._create_dummy_columns(input_feature_test_df)
-            input_feature_test_df = self._rename_columns(input_feature_test_df)
-            logging.info("Custom transformations applied to train and test data")
+        except Exception as e:
+            raise MyException(e, sys)
 
-            logging.info("Starting data transformation")
-            preprocessor = self.get_data_transformer_object()
-            logging.info("Got the preprocessor object")
+    def initiate_model_evaluation(self) -> ModelEvaluationArtifact:
+        """
+        Method Name :   initiate_model_evaluation
+        Description :   This function is used to initiate all steps of the model evaluation
+        
+        Output      :   Returns model evaluation artifact
+        On Failure  :   Write an exception log and then raise an exception
+        """  
+        try:
+            print("------------------------------------------------------------------------------------------------")
+            logging.info("Initialized Model Evaluation Component.")
+            evaluate_model_response = self.evaluate_model()
+            s3_model_path = self.model_eval_config.s3_model_key_path
 
-            logging.info("Initializing transformation for Training-data")
-            input_feature_train_arr = preprocessor.fit_transform(input_feature_train_df)
-            logging.info("Initializing transformation for Testing-data")
-            input_feature_test_arr = preprocessor.transform(input_feature_test_df)
-            logging.info("Transformation done end to end to train-test df.")
+            model_evaluation_artifact = ModelEvaluationArtifact(
+                is_model_accepted=evaluate_model_response.is_model_accepted,
+                s3_model_path=s3_model_path,
+                trained_model_path=self.model_trainer_artifact.trained_model_file_path,
+                changed_accuracy=evaluate_model_response.difference)
 
-            logging.info("Applying SMOTEENN for handling imbalanced dataset.")
-            smt = SMOTEENN(sampling_strategy="minority")
-            input_feature_train_final, target_feature_train_final = smt.fit_resample(
-                input_feature_train_arr, target_feature_train_df
-            )
-            input_feature_test_final, target_feature_test_final = smt.fit_resample(
-                input_feature_test_arr, target_feature_test_df
-            )
-            logging.info("SMOTEENN applied to train-test df.")
-
-            train_arr = np.c_[input_feature_train_final, np.array(target_feature_train_final)]
-            test_arr = np.c_[input_feature_test_final, np.array(target_feature_test_final)]
-            logging.info("feature-target concatenation done for train-test df.")
-
-            save_object(self.data_transformation_config.transformed_object_file_path, preprocessor)
-            save_numpy_array_data(self.data_transformation_config.transformed_train_file_path, array=train_arr)
-            save_numpy_array_data(self.data_transformation_config.transformed_test_file_path, array=test_arr)
-            logging.info("Saving transformation object and transformed files.")
-
-            logging.info("Data transformation completed successfully")
-            return DataTransformationArtifact(
-                transformed_object_file_path=self.data_transformation_config.transformed_object_file_path,
-                transformed_train_file_path=self.data_transformation_config.transformed_train_file_path,
-                transformed_test_file_path=self.data_transformation_config.transformed_test_file_path
-            )
-
+            logging.info(f"Model evaluation artifact: {model_evaluation_artifact}")
+            return model_evaluation_artifact
         except Exception as e:
             raise MyException(e, sys) from e
